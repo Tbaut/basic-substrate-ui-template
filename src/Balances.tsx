65aebc5e13022eb149054883ee037e6b178231c6
Thibaut <thibaut.sardan@gmail.com>
2019-07-08 17:18:47 +0200

    one account

--- src/Balances.tsx
+++ src/Balances.tsx
@@ -10,39 +10,39 @@ interface Props {
 export default function Balances(props: Props) {
   const {api, keyring} = props;
   const accounts = keyring.getPairs();
+  const account = accounts[0]
   const [balances, setBalances] = useState<{[index: string]: string }>({});
-
-  useEffect(() => {
-    const unsubscribes = accounts.map(async (account) => {
-      try {
-        await api.query.balances.freeBalance(account.address, (current) => {
+  
+  const unsubscribeFunc = async () => {
+    try {
+      return await api.query.balances.freeBalance(account.address, (current) => {
           setBalances(balances => {
             return {
               ...balances,
               [account.address]: current.toString()
             }
-          });
-        })
-      } catch (error) {
-        console.error(error);
-      }
-    });
+        })  
+      })
+     } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    const  unsubscribe = unsubscribeFunc() ;
       
-    return () => unsubscribes.length && unsubscribes.map(async (unsubscribe) => {
-      if (unsubscribe) await unsubscribe();
-    }) ;
+    return async () => await unsubscribe();
   },[]);
 
-  function renderAccountsWithBalances () {
-    return accounts.map((account, index) =>  {
-      return <p key={index}>{account.meta.name}: {account.address} balance: {balances && balances[account.address]}</p>;
-    });
+  function renderAccountWithBalances () {
+    return <p>{account.meta.name}: {account.address} balance: {balances && balances[account.address]}</p>;
+
   }
 
   return (
     <>
       <h1>Balances</h1>
-      {renderAccountsWithBalances()}
+      {renderAccountWithBalances()}
     </>
   )
 }
