7fdca10c8bf4c737890e544997989ba29c8187d3
Thibaut <thibaut.sardan@gmail.com>
2019-07-14 22:17:17 +0200

    balance of extension accounts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import testKeyring from '@polkadot/keyring/testing'
 import { Container} from 'semantic-ui-react';
+import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
+import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
 import 'semantic-ui-css/semantic.min.css'
 
 import Balances from './Balances'
@@ -11,20 +13,30 @@ import Transfer from './Transfer'
  export default function App () {
   const [api, setApi] = useState<ApiPromise>();
 
-  useEffect (()=>{
-    const getApi = async () => {
-      const provider = new WsProvider();
-      try {
-        // Create the API and wait until ready
-        const api = await ApiPromise.create(provider);
-        setApi(api);
-      } catch (e) {
-        console.error(e)
-      }
-    }
+  useEffect (() => {
+    const provider = new WsProvider();
 
-    getApi();
-  })
+    ApiPromise.create(provider)
+    .then((api) => setApi(api))
+    .catch((e)=> console.error(e));
+  },[])
+
+  const [injectedAccounts, setInjectedAccounts] = useState<InjectedAccountWithMeta[]>()
+
+  useEffect(() => {
+    web3Enable('basic substrate ui').then((extensions) => {
+      // this sends the request for connection
+      extensions.map((extension) => console.log('extension',extension))
+
+      // if the user accepts it the extension's array will contain something 
+      if (extensions.length){
+        web3Accounts().then((injectedAccounts) => {
+          setInjectedAccounts(injectedAccounts);
+          injectedAccounts.map((injectedAccount) => console.log('injectedAccount',injectedAccount))
+        });
+      }
+    });
+  },[]);
 
   if(!api || !api.isReady){
     return <div>Disconnected</div>
@@ -35,10 +47,12 @@ import Transfer from './Transfer'
       <Balances
         api={api}
         keyring={testKeyring()}
+        injected={injectedAccounts}
       />
       <Transfer
         api={api}
         keyring={testKeyring()}
+        // injected={injectedAccounts}
       />
     </Container>
   );
--- src/Balances.tsx
+++ src/Balances.tsx
@@ -2,23 +2,30 @@ import React, {useEffect, useState} from 'react';
 import { KeyringInstance } from '@polkadot/keyring/types';
 import { ApiPromise } from '@polkadot/api';
 import { Table } from 'semantic-ui-react';
-import { web3Accounts, web3Enable, web3FromAddress } from '@polkadot/extension-dapp';
-import { InjectedExtension, InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
+import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
 
 interface Props {
   api: ApiPromise,
-  keyring: KeyringInstance
+  keyring: KeyringInstance,
+  injected: InjectedAccountWithMeta[] | undefined
 };
 
 export default function Balances(props: Props) {
-  const {api, keyring} = props;
+  const {api, injected, keyring} = props;
   const accounts = keyring.getPairs();
+  const addresses = accounts.map(account => account.address);
+  const accountNames: string[] = accounts.map((account) => account.meta.name)
+
+  injected && injected.map((accountWithMeta) => {
+    accountNames.push(accountWithMeta.meta.name + ' (extension)');
+    addresses.push(accountWithMeta.address)
+  });
+  
   const [balances, setBalances] = useState<{[index: string]: string }>({});
 
   useEffect(() => {
     let unsubscribeAll : Function | undefined;
-    const addresses = accounts.map(account => account.address);
-  
+
     api.query.balances.freeBalance
       .multi(addresses, (currentBalances) => {
         currentBalances.map((balance, index) => 
@@ -35,39 +42,19 @@ export default function Balances(props: Props) {
       
     return () => unsubscribeAll && unsubscribeAll() ;
   },[]);
-
-  const [injectedBalances, setInjectedBalances] = useState<{[index: string]: string}>({})
-
-  useEffect(()=>{
-    let allInjected: InjectedExtension[];
-    let allInjectedAccounts: InjectedAccountWithMeta[];
-
-    web3Enable('my cool dapp').then((extensions: InjectedExtension[]) => {
-      extensions.map((extension) => {
-        console.log('extension',extension);
-      })
-
-      if (extensions.length){
-        web3Accounts().then((injectedAccounts) => {
-          injectedAccounts.map((injectedAccount) => {
-            console.log('injectedAccount',injectedAccount)
-          })
-        });
-      }
-    });
-  },[]);
-
+  
   return (
     <>
       <h1>Balances</h1>
       <Table celled striped> 
         <Table.Body>
-          {accounts.map((account,index) =>  {
+          {accountNames.map((name,index) =>  {
+            const address = addresses[index]
             return (
               <Table.Row key={index}>
-                <Table.Cell textAlign='right'>{account.meta.name}</Table.Cell>
-                <Table.Cell>{account.address}</Table.Cell>
-                <Table.Cell>{balances && balances[account.address]}</Table.Cell>  
+                <Table.Cell textAlign='right'>{name}</Table.Cell>
+                <Table.Cell>{address}</Table.Cell>
+                <Table.Cell>{balances && balances[address]}</Table.Cell>  
               </Table.Row>  
             )
           })}
